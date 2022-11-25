0326747523b86652ad68ae3555be0d06891f8158
Jason Gunawan <jasandgun@gmail.com>
2022-11-25 22:56:05 +0700

    add components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import TravelExploreIcon from '@mui/icons-material/TravelExplore';
+import CardTravelIcon from '@mui/icons-material/CardTravel';
+
+import { Search, SearchIconWrapper, StyledInputBase } from './HeaderStyles'
 
 function Header() {
   return (
-    <h1>Header</h1>
+    <Box sx={{ flexGrow:1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <CardTravelIcon />
+          </IconButton>
+          <Typography 
+            variant="h5" 
+            color="inherit"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Travel Advisor
+          </Typography>
+          {/* <Autocomplete> */}
+            <Search>
+              <SearchIconWrapper>
+                <TravelExploreIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Explore new places..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+          {/* </Autocomplete> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
